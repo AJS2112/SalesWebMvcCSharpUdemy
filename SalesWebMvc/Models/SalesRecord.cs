54e89e636e88e116ca614ca8ef73491184e855f5
Antonio Sucre <ajs2104@gmail.com>
2020-03-02 03:50:23 -0300

    Fixed Format

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -6,9 +6,9 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
-        [DisplayFormat (DataFormatString ="{0:dd/MM/yyyy")]
+        [DisplayFormat (DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
-        [DisplayFormat(DataFormatString = "{0:F2")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
